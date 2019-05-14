7d4d4ce44997d0fed1c9b4f99eeebc33473f3602
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-14 11:59:33 +0100

    remove console.log

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "@reach/router";
 
 const NavigationBar = ({ loggedInUser }) => {
-  console.log(loggedInUser);
   return (
     <nav>
       <Link to="/">Home</Link> | <Link to="/topics">Topics</Link> |{" "}
