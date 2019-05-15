21dc517739aff4c1561b4d129257bc19ff4e662a
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-15 12:11:11 +0100

    Sort by date/comments/votes

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "@reach/router";
 import { Button, Card } from "@blueprintjs/core";
+import moment from "moment";
 
 import fetchArticles from "../queries/fetchArticles";
 
 const ArticleList = ({ topic }) => {
   const [articles, setArticles] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
 
   useEffect(() => {
     let mounted = true;
 
     const fetchData = async () => {
-      const articles = await fetchArticles({ topic });
+      const articles = await fetchArticles({
+        topic,
+        sort_by: sortBy,
+        order: sortOrder
+      });
       mounted && setArticles(articles ? articles : []);
     };
     fetchData();
@@ -19,20 +26,58 @@ const ArticleList = ({ topic }) => {
     return () => {
       mounted = false;
     };
-  }, [topic]);
+  }, [topic, sortBy, sortOrder]);
+
+  const handleClick = sortKey => {
+    setSortBy(sortKey);
+    setSortOrder(sortOrder === "desc" ? "asc" : "desc");
+  };
 
   return (
     articles && (
       <div>
-        {articles.map(({ title, author, article_id }) => (
-          <Card interactive={true} className="article-list" key={article_id}>
-            <h3>{title}</h3>
-            <p>written by {author}</p>
-            <Link to={`/articles/${article_id}`}>
-              <Button>Read</Button>
-            </Link>
-          </Card>
-        ))}
+        <Card interactive={true} className="article-list">
+          <h4>
+            Sort by:{" "}
+            <Button
+              className="sort-button"
+              icon="numbered-list"
+              onClick={() => handleClick("votes")}
+            >
+              Votes
+            </Button>
+            <Button
+              className="sort-button"
+              icon="chat"
+              onClick={() => handleClick("comment_count")}
+            >
+              Comments
+            </Button>
+            <Button
+              className="sort-button"
+              icon="calendar"
+              onClick={() => handleClick("created_at")}
+            >
+              Date
+            </Button>
+          </h4>
+        </Card>
+        {articles.map(
+          ({ title, author, article_id, created_at, votes, comment_count }) => (
+            <Card interactive={true} className="article-list" key={article_id}>
+              <h3>
+                {title} (score: {votes}, comments: {comment_count})
+              </h3>
+              <p>
+                Written by {author} on{" "}
+                {moment(created_at).format("MMMM Do YYYY [at] h:mm a")}
+              </p>
+              <Link to={`/articles/${article_id}`}>
+                <Button>Read</Button>
+              </Link>
+            </Card>
+          )
+        )}
       </div>
     )
   );
