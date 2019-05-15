e8fd5585fcbfac2143ddc74ab10ddb83d3b2247f
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-15 16:35:53 +0100

    Remove navbar button text if screen width 400px or less

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,30 +2,40 @@ import React from "react";
 import { Navbar, Alignment, Button } from "@blueprintjs/core";
 import { Link } from "@reach/router";
 
-const NavBar = ({ loggedInUser, setLoggedInUser , setShowLoginForm}) => {
+const NavBar = ({ loggedInUser, setLoggedInUser, setShowLoginForm }) => {
   return (
     <Navbar>
       <Navbar.Group align={Alignment.LEFT}>
         <Navbar.Heading>NC News</Navbar.Heading>
         <Navbar.Divider />
         <Link to="/">
-          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="home">
+            <span className="button-text">Home</span>
+          </Button>
         </Link>
         <Link to="/topics">
-          <Button className="bp3-minimal" icon="folder-open" text="Topics" />
+          <Button className="bp3-minimal" icon="folder-open">
+            <span className="button-text">Topics</span>
+          </Button>
         </Link>
         {loggedInUser ? (
           <Button
             className="bp3-minimal"
             icon="log-out"
-            text={`Logout (${loggedInUser})`}
             onClick={e => {
               e.preventDefault();
               setLoggedInUser(null);
             }}
-          />
+          >
+            <span className="button-text">{`Logout (${loggedInUser})`}</span>
+          </Button>
         ) : (
-          <Button className="bp3-minimal" icon="log-in" text="Login" onClick={()=> setShowLoginForm(true)}/>
+          <Button
+            className="bp3-minimal"
+            icon="log-in"
+            text="Login"
+            onClick={() => setShowLoginForm(true)}
+          />
         )}
       </Navbar.Group>
     </Navbar>
