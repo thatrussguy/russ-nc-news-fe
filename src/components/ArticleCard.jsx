29b3c935a3ff0a8833f356656386d9bfd80196f6
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-16 21:20:27 +0100

    tidy up ArticleList

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,35 @@
+import moment from "moment";
+import React from "react";
+import { Button, Card } from "@blueprintjs/core";
+import { Link } from "@reach/router";
+
+const ArticleCard = ({
+  article: { article_id, author, comment_count, created_at, title, votes },
+  handleDeleteClick,
+  loggedInUser
+}) => {
+  return (
+    <Card interactive={true} className="article-list" key={article_id}>
+      <h3>
+        {title} (score: {votes}, comments: {comment_count})
+      </h3>
+      <p>
+        Written by <Link to={`/${author}/articles`}>{author}</Link> on{" "}
+        {moment(created_at).format("MMMM Do YYYY [at] h:mm a")}
+      </p>
+      {loggedInUser === author && (
+        <Button
+          onClick={() => handleDeleteClick(article_id)}
+          className="article-button"
+        >
+          Delete
+        </Button>
+      )}
+      <Link to={`/articles/${article_id}`}>
+        <Button className="article-button">Read</Button>
+      </Link>
+    </Card>
+  );
+};
+
+export default ArticleCard;
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,36 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "@reach/router";
-import { Button, Card, Spinner } from "@blueprintjs/core";
-import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { Button, Spinner } from "@blueprintjs/core";
 
+import ArticleCard from "./ArticleCard";
 import ArticleForm from "./ArticleForm";
-import PaginationFooter from "./PaginationFooter";
 import ErrorCard from "../components/ErrorCard";
+import PaginationFooter from "./PaginationFooter";
 
 import fetchArticles from "../queries/fetchArticles";
 import deleteArticle from "../queries/deleteArticle";
 
-const ArticleList = ({ topic, loggedInUser, author }) => {
-  console.log();
-
+const ArticleList = ({ author, loggedInUser, topic }) => {
   const [articles, setArticles] = useState(null);
-  const [totalCount, setTotalCount] = useState(0);
-  const [sortBy, setSortBy] = useState(null);
-  const [sortOrder, setSortOrder] = useState(null);
-  const [showArticleForm, setShowArticleForm] = useState(false);
-  const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState(null);
+  const [showArticleForm, setShowArticleForm] = useState(false);
+  const [sortOrder, setSortOrder] = useState(null);
+  const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
     let mounted = true;
 
     const fetchData = async () => {
       const response = await fetchArticles({
-        topic,
-        sort_by: sortBy,
+        author,
         order: sortOrder,
         p: page,
-        author
+        sort_by: sortBy,
+        topic
       }).catch(({ response: { data: { message }, status } }) =>
         setError({ status, message })
       );
@@ -46,14 +43,13 @@ const ArticleList = ({ topic, loggedInUser, author }) => {
     };
   }, [topic, sortBy, sortOrder, page, author]);
 
-  const handleClick = sortKey => {
-    setSortBy(sortKey);
-    setSortOrder(sortOrder === "desc" ? "asc" : "desc");
-  };
-
   const handleDeleteClick = article_id => {
     deleteArticle(article_id);
     setArticles(articles.filter(article => article.article_id !== article_id));
+  };
+  const handleSortClick = sortKey => {
+    setSortBy(sortKey);
+    setSortOrder(sortOrder === "desc" ? "asc" : "desc");
   };
 
   return (
@@ -62,17 +58,17 @@ const ArticleList = ({ topic, loggedInUser, author }) => {
         {topic ? topic : author ? `${author}'s` : "All"} articles
       </h1>
       <Button
-        disabled={!loggedInUser}
         className="article"
-        text={loggedInUser ? "Write an article" : "Login to write an article"}
+        disabled={!loggedInUser}
         onClick={() => setShowArticleForm(true)}
+        text={loggedInUser ? "Write an article" : "Login to write an article"}
       />
       {showArticleForm && (
         <ArticleForm
-          loggedInUser={loggedInUser}
-          setShowArticleForm={setShowArticleForm}
           articles={articles}
+          loggedInUser={loggedInUser}
           setArticles={setArticles}
+          setShowArticleForm={setShowArticleForm}
           topic={topic}
         />
       )}
@@ -81,63 +77,44 @@ const ArticleList = ({ topic, loggedInUser, author }) => {
         <Button
           className="sort-button bp3-minimal"
           icon="heart"
-          onClick={() => handleClick("votes")}
+          onClick={() => handleSortClick("votes")}
         >
           <span className="button-text">Votes</span>
         </Button>
         <Button
           className="sort-button bp3-minimal"
           icon="chat"
-          onClick={() => handleClick("comment_count")}
+          onClick={() => handleSortClick("comment_count")}
         >
           <span className="button-text">Comments</span>
         </Button>
         <Button
           className="sort-button bp3-minimal"
           icon="calendar"
-          onClick={() => handleClick("created_at")}
+          onClick={() => handleSortClick("created_at")}
         >
           <span className="button-text">Date</span>
         </Button>
       </h4>
       {articles ? (
-        articles.map(
-          ({ title, author, article_id, created_at, votes, comment_count }) => (
-            <Card interactive={true} className="article-list" key={article_id}>
-              <h3>
-                {title} (score: {votes}, comments: {comment_count})
-              </h3>
-              <p>
-                Written by <Link to={`/${author}/articles`}>{author}</Link> on{" "}
-                {moment(created_at).format("MMMM Do YYYY [at] h:mm a")}
-              </p>
-              {loggedInUser === author && (
-                <Button
-                  onClick={() => handleDeleteClick(article_id)}
-                  className="article-button"
-                >
-                  Delete
-                </Button>
-              )}
-              <Link to={`/articles/${article_id}`}>
-                <Button className="article-button">Read</Button>
-              </Link>
-            </Card>
-          )
-        )
+        articles.map(article => (
+          <ArticleCard
+            article={article}
+            handleDeleteClick={handleDeleteClick}
+            loggedInUser={loggedInUser}
+          />
+        ))
       ) : error ? (
         <ErrorCard error={error} />
       ) : (
         <Spinner className="article" />
       )}
-      {totalCount ? (
+      {totalCount && (
         <PaginationFooter
           page={page}
           setPage={setPage}
           totalPages={Math.ceil(totalCount / 10)}
         />
-      ) : (
-        ""
       )}
     </div>
   );
