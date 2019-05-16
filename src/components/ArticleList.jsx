9740842cf2205b837d28b4eafeeb257435e0a66f
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-16 21:36:55 +0100

    tidy ArticleList component

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -5,6 +5,7 @@ import ArticleCard from "./ArticleCard";
 import ArticleForm from "./ArticleForm";
 import ErrorCard from "../components/ErrorCard";
 import PaginationFooter from "./PaginationFooter";
+import SortButtons from "./SortButtons";
 
 import deleteArticle from "../queries/deleteArticle";
 import fetchArticles from "../queries/fetchArticles";
@@ -72,30 +73,7 @@ const ArticleList = ({ author, loggedInUser, topic }) => {
           topic={topic}
         />
       )}
-      <h4 className="article-list">
-        Sort by:{" "}
-        <Button
-          className="sort-button bp3-minimal"
-          icon="heart"
-          onClick={() => handleSortClick("votes")}
-        >
-          <span className="button-text">Votes</span>
-        </Button>
-        <Button
-          className="sort-button bp3-minimal"
-          icon="chat"
-          onClick={() => handleSortClick("comment_count")}
-        >
-          <span className="button-text">Comments</span>
-        </Button>
-        <Button
-          className="sort-button bp3-minimal"
-          icon="calendar"
-          onClick={() => handleSortClick("created_at")}
-        >
-          <span className="button-text">Date</span>
-        </Button>
-      </h4>
+      <SortButtons handleSortClick={handleSortClick} />
       {articles ? (
         articles.map(article => (
           <ArticleCard
---
+++ src/components/SortButtons.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "@blueprintjs/core";
+
+const SortButtons = ({ handleSortClick }) => {
+  return (
+    <h4 className="article-list">
+      Sort by:{" "}
+      <Button
+        className="sort-button bp3-minimal"
+        icon="heart"
+        onClick={() => handleSortClick("votes")}
+      >
+        <span className="button-text">Votes</span>
+      </Button>
+      <Button
+        className="sort-button bp3-minimal"
+        icon="chat"
+        onClick={() => handleSortClick("comment_count")}
+      >
+        <span className="button-text">Comments</span>
+      </Button>
+      <Button
+        className="sort-button bp3-minimal"
+        icon="calendar"
+        onClick={() => handleSortClick("created_at")}
+      >
+        <span className="button-text">Date</span>
+      </Button>
+    </h4>
+  );
+};
+
+export default SortButtons;
