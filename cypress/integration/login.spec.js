211ecbd0ea363ff1f68883389f9bfd1a87b48391
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-05-18 18:53:55 +0100

    Reword test description

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,19 +1,17 @@
-describe("NC News", () => {
+describe("Logging in", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  describe("Logging in", () => {
-    it("logs in with a valid username", () => {
-      cy.get('[data-cy="navbar-login-button"]').click();
-      cy.get('[data-cy="username-input"]').type("guest");
-      cy.get('[data-cy="form-login-button"]').click();
-      cy.get('[data-cy="navbar-logout-button"]');
-    });
-    it("doesn't log in with an invalid username", () => {
-      cy.get('[data-cy="navbar-login-button"]').click();
-      cy.get('[data-cy="username-input"]').type("unwelcomeguest");
-      cy.get('[data-cy="form-login-button"]').click();
-      cy.get('[data-cy="login-form"]').contains("Invalid username");
-    });
+  it("logs in with a valid username", () => {
+    cy.get('[data-cy="navbar-login-button"]').click();
+    cy.get('[data-cy="username-input"]').type("guest");
+    cy.get('[data-cy="form-login-button"]').click();
+    cy.get('[data-cy="navbar-logout-button"]');
+  });
+  it("doesn't log in with an invalid username", () => {
+    cy.get('[data-cy="navbar-login-button"]').click();
+    cy.get('[data-cy="username-input"]').type("unwelcomeguest");
+    cy.get('[data-cy="form-login-button"]').click();
+    cy.get('[data-cy="login-form"]').contains("Invalid username");
   });
 });
